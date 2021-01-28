2e5e10f086e0eb5d01cc9c6461934350c34e4c75
Nahuel Segovia <nahuel@gcoop.coop>
2021-01-28 18:44:46 -0300

    creando token a partir del login

--- controllers/SearchUserController.js
+++ controllers/SearchUserController.js
@@ -1,19 +1,36 @@
 const mongo = require('../database/connect');
 let user = require('../database/models/User');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 class SearchUserController{
     constructor(){}
 
     static async userLogin(email, password, res){
         let userByEmail = await user.findOne({email : email});
-        if(userByEmail){
-           let showuser = await bcrypt.compare(password, userById.password);
-           res.send(showuser);
+        if(!userByEmail){
+            res.json({'message': 'user not found'});
         }
 
         else{
-            res.json({'message': 'user not found'});
+            let userPassword = await bcrypt.compare(password, userByEmail.password);
+            if(!userPassword){
+                res.json({'message': 'incorrect password'})
+            }
+
+            else{
+               const payload = {
+                   check: true
+               } 
+               const token = jwt.sign(payload, 'llavecitaenlapancita', {
+                expiresIn: 1440
+               });
+
+               res.json({
+                   'message': 'Authenticated - 200',
+                   'token': token
+               })
+            }
         }
     }
 
