f09fa8ab9afe4d5576768bbe0ff43384e0dd5c59
Nahuel Segovia <nahuel@gcoop.coop>
2021-01-30 15:31:14 -0300

    PostController creado

---
+++ controllers/PostController.js
@@ -0,0 +1,14 @@
+const mongo = require('../database/connect');
+let user = require('../database/models/User');
+const bcrypt = require('bcrypt');
+
+class PostController{
+    constructor(){}
+
+create(title, content, hashtag, user_id){
+        return 'Work In Progress'
+    }
+}
+
+
+module.exports = RegisterController;
