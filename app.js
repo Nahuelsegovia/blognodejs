4c33eeeea5c2a21312e2e52cc26100d8fa5a62f0
Nahuel Segovia <nahuel@gcoop.coop>
2021-01-30 16:19:06 -0300

    usando body-parser en la aplicación

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+let bodyParser = require('body-parser')
 
 var indexRouter = require('./routes/index');
 
@@ -21,6 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
 
 app.use('/', indexRouter);
 
