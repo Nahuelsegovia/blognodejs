89d0ccf20e8ed05f0ba701c8d8775282ca649e52
Nahuel Segovia <nahuel@gcoop.coop>
2021-01-30 19:36:09 -0300

    Bugs fix

--- database/models/Post.js
+++ database/models/Post.js
@@ -3,13 +3,23 @@ const mongoose = require('mongoose');
 let post = mongoose.Schema({
     title: {
         type: String,
+        required: true,
     },
     content:{
         type: String,
+        required: true,
     },
-    post_date: Date,
-    id: Number,
-    user_id: Number,
+    user_id: {
+        type: Number,
+        required: true
+    },
+    post_date: {
+        type: Date,
+        required: true,
+    },
+    id: {
+        type: Number,
+    },
 })
 
 module.exports = mongoose.model('Post', post);
