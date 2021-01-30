62570751022cfb11b5b1096a966cb0d78024b0e8
Nahuel Segovia <nahuel@gcoop.coop>
2021-01-30 19:35:24 -0300

    Controlador de post completo

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,14 +1,28 @@
 const mongo = require('../database/connect');
-let user = require('../database/models/User');
+let post = require('../database/models/Post');
 const bcrypt = require('bcrypt');
 
 class PostController{
     constructor(){}
 
-create(title, content, hashtag, user_id){
-        return 'Work In Progress'
+static create(title, content, hashtag, user_id, res){
+
+    let post = new post({
+        title: title,
+        content: content,
+        hashtag: hashtag,
+        user_id: user_id,
+        post_date: Date.now()
+    })
+
+    post.save().then( () => {
+        res.json({
+            message: `post ${post.title} created successfully`
+        })
+    })
+    
     }
 }
 
 
-module.exports = RegisterController;
+module.exports = PostController;
