167c86a485ae4c42417035ffbb2e025b55721cd8
Nahuel Segovia <nahuel@gcoop.coop>
2021-01-31 15:04:39 -0300

    Dejé solo las rutas de vistas y la de logueo y deslogueo

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,11 @@ const SearchUserController = require('../controllers/SearchUserController');
 const PostController = require('../controllers/PostController');
 
 router.get('/user', function(req, res, next){
-  res.render('index.html', {'name': 'caca'})//req.params.name});
+  res.render('index.html', {'name': req.params.name});
+})
+
+router.get('/login', function(req, res, next){
+  res.render('login.html');
 })
 
 
@@ -30,25 +34,5 @@ router.post('/register', function(req, res, next) {
   let password = req.query.password;
   let create = user.create(email, password, res);
 });
-
-
-
-/*Post area*/
-router.post('/create/post', function(req, res, next){
-  if(req.session.admin){
-    let title = req.body.title;
-    let content = req.body.content;
-    let user_id = req.body.user_id;
-    let hashtag = req.body.hashtag;
-
-    let posteo = PostController.create(title, content, hashtag, user_id, res);
-    return posteo;
-  }
-
-  else{
-    res.send('sin permiso');
-  }
-
-})
 
 module.exports = router;
