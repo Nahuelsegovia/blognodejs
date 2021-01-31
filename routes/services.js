ce0b4ea6461cf65cde493e28cb316988fc9bcd5d
Nahuel Segovia <nahuel@gcoop.coop>
2021-01-31 15:04:55 -0300

    Nueva ruta de servicios

---
+++ routes/services.js
@@ -0,0 +1,70 @@
+var express = require('express');
+var router = express.Router();
+const PostController = require('../controllers/PostController');
+let postModel = require('../database/models/Post');
+
+/*Post area*/
+router.post('/create/post', function(req, res, next){
+    if(req.session.admin){
+      let title = req.body.title;
+      let content = req.body.content;
+      let user_id = req.body.user_id;
+      let hashtag = req.body.hashtag;
+  
+      let posteo = PostController.create(title, content, hashtag, user_id, res);
+      return posteo;
+    }
+  
+    else{
+      res.status(403).send({message: 'Upss, Forbidden'})
+    }
+  
+  })
+
+  router.post('/delete/post/:id', function(req, res, next){
+    if(req.session.admin){
+        let post = postModel.remove({_id:req.params.id}, (err) => {
+            if(err){
+                res.send(err);
+            }
+    
+            else{
+                res.json({
+                    status: '200',
+                    message: `post ${req.params.id} deleted`,
+                });
+            }
+        });
+    }
+  
+    else{
+      res.status(403).send({message: 'Upss, Forbidden'})
+    }
+  
+  })
+
+router.get('/post/all', function(req, res, next){
+    let posts = postModel.find({}, (err, result) => {
+        if(err){
+            res.send(err);
+        }
+
+        else{
+            res.send(result);
+        }
+    })
+})
+
+router.get('/post/:id', function(req, res, next){
+    let post = postModel.findById(req.params.id, (err, result) => {
+        if(err){
+            res.send(err);
+        }
+
+        else{
+            res.send(result);
+        }
+    } );
+});
+
+module.exports = router;
